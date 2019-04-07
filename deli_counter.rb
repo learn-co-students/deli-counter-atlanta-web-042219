8987f34930fe622b88448c70935053b4ae56b07d
han xu <hanxu27@gmail.com>
2019-04-07 05:18:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ def line(arr)
   if arr == []
     puts "The line is currently empty."
   else
-    puts "The line is currently:"
+    puts "The line is currently:".chomp
     
     arr.each_with_index { |name, index| 
     puts " #{index+1}. #{name}"
