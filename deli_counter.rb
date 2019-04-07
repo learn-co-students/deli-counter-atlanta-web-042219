e53c6f7da12d2d3b63aef3c9ab990487199d4776
han xu <hanxu27@gmail.com>
2019-04-07 05:15:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ end
 
 def now_serving(arr)
   if arr == []
-    puts "The line is currently empty."
+    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{arr.shift}."
   end
